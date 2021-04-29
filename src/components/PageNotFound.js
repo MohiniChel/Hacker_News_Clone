c55cc25d47184d37763c2c28d6edb174305fc4a6
mohini <babychel@gmail.com>
2021-04-29 21:03:20 +0530

    hacker_news

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+    return(
+        <p> Page Not found. Go to <Link to="/"> Home </Link>
+        </p>
+    );
+};
+
+export default PageNotFound;
